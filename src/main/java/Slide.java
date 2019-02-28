0592600e3a14a6b3492d728c8cdc679be76a9108
Jonas Hübotter <me@jonhue.me>
2019-02-28 19:51:55 +0100

    rm counts

--- src/main/java/Slide.java
+++ src/main/java/Slide.java
@@ -1,9 +1,6 @@
 public interface Slide extends Comparable<Slide> {
     public int[] getIDs();
 
-    public int getTagCount();
-    public void setTagCount(int tagCount);
-
     public String[] getTags();
     public void setTags(String[] tags);
 
--- src/main/java/VertPicture.java
+++ src/main/java/VertPicture.java
@@ -1,9 +1,6 @@
 public interface VertPicture extends Comparable<VertPicture> {
     public int getID();
 
-    public int getTagCount();
-    public void setTagCount(int tagCount);
-
     public String[] getTags();
     public void setTags(String[] tags);
 
