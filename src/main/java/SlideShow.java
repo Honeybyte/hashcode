f7739f0cd4501387ffa5eded88eb62ab5520be89
bjoernboss <bjoernbossh@gmail.com>
2019-02-28 20:30:58 +0100

    implemented slideshow-class

--- src/main/java/SlideShow.java
+++ src/main/java/SlideShow.java
@@ -27,25 +27,67 @@ public class SlideShow {
         show.add(slides.get(slides.size() - 1));
         slides.remove((slides.size() - 1));
         while(slides.size() > 0){
-            bestIndex[0] = 0;
-            bestIndex[1] = 0;
-            bestValue[0] = 0;
-            bestValue[1] = 0;
-            for(int i = 0; i < slides.size(); i++){
+            bestIndex[0] = -1;
+            bestIndex[1] = -1;
+            bestValue[0] = -1;
+            bestValue[1] = -1;
+            for(int i = slides.size() - 1; i >= 0; i--){
 
                 //check if the current slide suits the left-side of the slideshow or the right-side
                 int resLeft = slides.get(i).compareTo(show.get(0));
                 int resRight = slides.get(i).compareTo(show.get(show.size() - 1));
                 if(resLeft > resRight) {
-
+                    if(resLeft >= bestValue[0]) {
+                        bestValue[0] = resLeft;
+                        bestIndex[0] = i;
+                    }
+                    else if(resRight >= bestValue[1]) {
+                        bestValue[1] = resRight;
+                        bestIndex[1] = i;
+                    }
                 }
                 else if(resRight > resLeft) {
-
+                    if(resRight >= bestValue[1]) {
+                        bestValue[1] = resRight;
+                        bestIndex[1] = i;
+                    }
+                    else if(resLeft >= bestValue[0]) {
+                        bestValue[0] = resLeft;
+                        bestIndex[0] = i;
+                    }
                 }
                 else {
-
+                    //both sides are equally suited
+                    if(resLeft < bestValue[0]){
+                        if(resLeft >= bestValue[1]){
+                            bestValue[1] = resLeft;
+                            bestIndex[i] = i;
+                        }
+                    }
+                    else if(resLeft < bestValue[1]){
+                        bestValue[0] = resLeft;
+                        bestIndex[0] = i;
+                    }
+                    else {
+                        if(bestValue[0] <= bestValue[1]){
+                            bestValue[0] = resLeft;
+                            bestIndex[0] = i;
+                        }
+                        else {
+                            bestValue[1] = resLeft;
+                            bestIndex[1] = i;
+                        }
+                    }
                 }
             }
+
+            //add the slides to the output and remove them from the input
+            if(bestIndex[0] != -1)
+                show.add(0, slides.get(bestIndex[0]));
+            if(bestIndex[1] != -1)
+                show.add(slides.get(bestIndex[1]));
+
+            //remove
         }
         return show;
     }
