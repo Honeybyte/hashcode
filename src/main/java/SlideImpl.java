3ad938838fca43e11d14e84ba0f34c0192e158cc
Simon Schnückel <48095256+AddUsernamePlease@users.noreply.github.com>
2019-02-28 20:52:40 +0100

    File parse (#6)
    * first
    
    * SlidesImpl firstPush
    
    * AllDone
    
    * AllDone
    
    * AllDone

---
+++ src/main/java/SlideImpl.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SlideImpl implements Slide{
+
+    public List<String> tags;
+    private int[] IDs;
+
+    public SlideImpl(ArrayList<String> tags, int[] IDs){
+        this.IDs = IDs;
+        this.tags = tags;
+    }
+
+    @Override
+    public int[] getIDs() {
+        return this.IDs;
+    }
+
+    @Override
+    public List<String> getTags() {
+        return this.tags;
+    }
+
+    @Override
+    public int compareTo(Slide slide) {
+        return 0;
+    }
+}
---
+++ src/main/java/VertPictureImpl.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class VertPictureImpl implements VertPicture {
+    private List<String> tags = new ArrayList<>();
+    private int ID;
+
+    public VertPictureImpl (int ID){
+        this.ID = ID;
+    }
+
+    @Override
+    public int getID() {
+        return this.ID;
+    }
+
+    @Override
+    public List<String> getTags() {
+        return this.tags;
+    }
+
+    @Override
+    public int compareTo(VertPicture picture) {
+        return 0;
+    }
+}
